1c6bba3be36ffe4a3a7f3a801d10f88bd4b5f91c
kimray912 <kimray912@gmail.com>
2026-07-17 19:27:58 +0900

    CSRF 신뢰 도메인 추가

--- afleme_project/settings.py
+++ afleme_project/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://afleme.onrender.com']
 
 # Application definition
 
